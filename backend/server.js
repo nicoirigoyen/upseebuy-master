4d91acdab76aac919a5caac4f6ad1c15705dadd8
nicoirigoyen <nico.irigoyen.terre@hotmail.com>
2021-08-19 15:57:30 -0300

    asd

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,19 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (req,res)=>{
-    res.send('server is ready')
-});
+app.get('/api/products/:id', (req, res) => {
+    const product = data.products.find((x) => x._id === req.params.id);
+    if (product) {
+      res.send(product);
+    } else {
+      res.status(404).send({ message: 'Product Not Found' });
+    }
+  });
+
+  app.get('/api/products', (req, res) => {
+    res.send(data.products);
+  });
+  
 app.listen(5000, ()=>{
     console.log('server listening at port 5000')
 });
