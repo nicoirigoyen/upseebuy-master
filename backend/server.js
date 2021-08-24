0c829748a614150124a409512ab7ff9820e901a3
nicoirigoyen <nico.irigoyen.terre@hotmail.com>
2021-08-24 11:04:45 -0300

    implementSing-in Backend

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,20 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import userRouter from '../backend/routers/userRouter.js';
+import productRouter from '../backend/routers/productRouter.js';
+import dotenv from 'dotenv';
 
+dotenv.config();
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//generamos la conexion
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/amazona', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
 
 app.get('/api/products/:id', (req, res) => {
     const product = data.products.find((x) => x._id === req.params.id);
@@ -14,7 +28,17 @@ app.get('/api/products/:id', (req, res) => {
   app.get('/api/products', (req, res) => {
     res.send(data.products);
   });
+
+  app.use('/api/users', userRouter); 
+  app.use('/api/products', productRouter)
+  app.get('/', (req, res) => {
+    res.send('Server is ready!');
+  });
+ 
+  app.use((err, req, res, next) =>{
+    res.status(500).send({message: err.message})
+  })
   
-app.listen(5000, ()=>{
+app.listen(process.env.PORT || 5000, ()=>{
     console.log('server listening at port 5000')
 });
